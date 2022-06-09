a142fbdac53ba38f9ae4bee51bfdb472d39e15b2
2389451262@qq.com <2389451262@qq.com>
2022-06-09 14:41:52 +0800

    init commit.

---
+++ src/layout/Content/Content.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Aside from '../Aside/Aside'
+import Main from '../Main/Main'
+const Content = () => {
+	return <>
+		<div className='content'>
+			<Aside></Aside>
+			<Main></Main>
+		</div>
+	</>
+}
+
+export default Content
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Header from './layout/Header/Header'
+import Content from './layout/Content/Content'
+import './index.css'
+import './styles/layout/layout.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div className='container'>
+      <Header></Header>
+      <Content></Content>
+    </div>
+  </React.StrictMode>
+)
